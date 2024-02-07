0ec00c37343a912cba10b6ee670755f4e4ca0d70
NeerajK007 <neeraj.kumar32@wipro.com>
2024-02-07 17:06:55 +0530

    file sync - codeql - security bug - Building a command line with string concatenation

--- src/main/java/com/iemr/mmu/service/fileSync/FileSyncServiceImpl.java
+++ src/main/java/com/iemr/mmu/service/fileSync/FileSyncServiceImpl.java
@@ -26,6 +26,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,39 +45,38 @@ import com.iemr.mmu.utils.response.OutputResponse;
 
 @Service
 @PropertySource("classpath:application.properties")
-public class FileSyncServiceImpl implements FileSyncService{
-	
+public class FileSyncServiceImpl implements FileSyncService {
+
 	private final static Logger logger = LoggerFactory.getLogger(FileSyncServiceImpl.class);
-	
+
 	private static HttpUtils httpUtils = new HttpUtils();
-	
+
 	@Value("${serverIP}")
 	private String serverIP;
-	
+
 	@Value("${serverDomain}")
 	private String serverDomain;
-	
+
 	@Value("${serverUserName}")
 	private String serverUserName;
-	
+
 	@Value("${serverPassword}")
 	private String serverPassword;
-	
+
 	@Value("${getServerCredentialURL}")
 	private String getServerCredentialURL;
-	
+
 	@Value("${localFolderToSync}")
 	private String localFolderToSync;
-	
+
 	@Value("${serverFolder}")
 	private String serverFolder;
-	
+
 	private static String fileSynclogspath = "./fileSynclogs";
-	
-	static
-	{
+
+	static {
 		File f = new File(fileSynclogspath);
-		System.out.println("Log Folder Path - "+ f.getAbsolutePath());
+		System.out.println("Log Folder Path - " + f.getAbsolutePath());
 		if (!f.exists()) {
 			f.mkdir();
 			logger.info("Log Folder created for File Sync activity");
@@ -86,126 +86,129 @@ public class FileSyncServiceImpl implements FileSyncService{
 	@Override
 	public String getServerCredential() {
 
-		Map<String,String> map = new HashMap<String,String>();
-		
+		Map<String, String> map = new HashMap<String, String>();
+
 		map.put("serverIP", serverIP);
 		map.put("serverDomain", serverDomain);
 		map.put("serverUserName", serverUserName);
 		map.put("serverPassword", serverPassword);
-		
-		String jsonResult= new Gson().toJson(map);
-		
+
+		String jsonResult = new Gson().toJson(map);
+
 		return jsonResult;
 	}
 
-	
 	@Override
 	public String syncFiles(String ServerAuthorization) throws IEMRException, IOException {
 		logger.info("File Sync activity Started");
 		HashMap<String, Object> header = new HashMap<>();
-		if (ServerAuthorization != null)
-		{
+		if (ServerAuthorization != null) {
 			header.put("Authorization", ServerAuthorization);
 		}
 
-		String result = httpUtils.get(getServerCredentialURL,header);
-		
+		String result = httpUtils.get(getServerCredentialURL, header);
+
 		OutputResponse identityResponse = InputMapper.gson().fromJson(result, OutputResponse.class);
-		if (identityResponse.getStatusCode() == OutputResponse.USERID_FAILURE)
-		{
+		if (identityResponse.getStatusCode() == OutputResponse.USERID_FAILURE) {
 			throw new IEMRException(identityResponse.getErrorMessage());
 		}
-		String data=  identityResponse.getData();
+		String data = identityResponse.getData();
 		ServerCredential serverCredential = InputMapper.gson().fromJson(data, ServerCredential.class);
-		
-		String IP=serverCredential.getServerIP();
-		String Domain=serverCredential.getServerDomain();
-		String UserName=serverCredential.getServerUserName();
-		String Password=serverCredential.getServerPassword();
-		
-		
-		String logFileName= "/fileSync.log." +System.currentTimeMillis();
-		
-		String createAccessCommand= "net use \\\\"+IP+"\\IPC$ /u:"+Domain+"\\"+UserName+" "+Password;
-		String createCopyCommand= "robocopy "+localFolderToSync+" \\\\"+IP+"\\"+serverFolder+"  /E  /R:3 /log+:" + fileSynclogspath + logFileName;
-		String deleteAccessCommand= "net use \\\\"+IP+"\\IPC$ /delete";
-		
-		logger.info("fileSync createAccessCommand - >"+ createAccessCommand);
-		logger.info("fileSync createCopyCommand - >"  + createCopyCommand);
-		logger.info("fileSync deleteAccessCommand - >"+ deleteAccessCommand);
-		
+
+		String IP = serverCredential.getServerIP();
+		String Domain = serverCredential.getServerDomain();
+		String UserName = serverCredential.getServerUserName();
+		String Password = serverCredential.getServerPassword();
+
+		String logFileName = "/fileSync.log." + System.currentTimeMillis();
+
+		// String createAccessCommand= "net use \\\\"+IP+"\\IPC$
+		// /u:"+Domain+"\\"+UserName+" "+Password;
+		// String createCopyCommand= "robocopy "+localFolderToSync+"
+		// \\\\"+IP+"\\"+serverFolder+" /E /R:3 /log+:" + fileSynclogspath +
+		// logFileName;
+		// String deleteAccessCommand= "net use \\\\"+IP+"\\IPC$ /delete";
+
+		String[] createAccessCommandArr = new String[] { "net use \\\\", IP, "\\IPC$ /u:", Domain, "\\", UserName, " ",
+				Password };
+		String[] createCopyCommandArr = new String[] { "robocopy ", localFolderToSync, " \\\\", IP, "\\", serverFolder,
+				"  /E  /R:3 /log, :", fileSynclogspath, logFileName };
+		String[] deleteAccessCommandArr = new String[] { "net use \\\\", IP, "\\IPC$ /delete" };
+
+		logger.info(
+				"fileSync createAccessCommand - >" + Arrays.stream(createAccessCommandArr).reduce("", String::concat));
+		logger.info("fileSync createCopyCommand - >" + Arrays.stream(createCopyCommandArr).reduce("", String::concat));
+		logger.info(
+				"fileSync deleteAccessCommand - >" + Arrays.stream(deleteAccessCommandArr).reduce("", String::concat));
+
 		Runtime rt = Runtime.getRuntime();
 		FileInputStream fstream = null;
-		 
-		try 
-		{
-			Process pr1= rt.exec(createAccessCommand);
-			Process pr2= rt.exec(createCopyCommand);
-			Process pr3= rt.exec(deleteAccessCommand);
-			
+
+		try {
+			Process pr1 = rt.exec(createAccessCommandArr);
+			Process pr2 = rt.exec(createCopyCommandArr);
+			Process pr3 = rt.exec(deleteAccessCommandArr);
+
 			logger.info(System.getProperty("user.dir"));
-			
+
 			try {
-				 logger.info("File Sync activity sleeping");
+				logger.info("File Sync activity sleeping");
 				Thread.sleep(35000);
-				 logger.info("File Sync activity wakeup");
+				logger.info("File Sync activity wakeup");
 			} catch (InterruptedException e) {
 				logger.error(e.getMessage());
-				 Thread.currentThread().interrupt();
+				Thread.currentThread().interrupt();
 			}
-			
-			   fstream = new FileInputStream(fileSynclogspath.substring(2) + logFileName);
-		
-			    try (BufferedReader br = new BufferedReader(new InputStreamReader(fstream))) {
-			   
-			   String strLine;
-			   /* read log line by line */
-			   while ((strLine = br.readLine()) != null)   {
-			     /* parse strLine to obtain what you want to read*/
-			     if(strLine.contains("ERROR"))
-			     {
-			    	 pr1.destroy();
-			    	 pr2.destroy();
-			    	 pr3.destroy();
-			    	 
-			    	 logger.info("File Sync activity Failed. Log File Name - "+logFileName.substring(1));
-			    	 return "File Sync activity Failed. Log File Name - "+logFileName.substring(1);
-			     }
-			    
-			   }
-			  
-			 }
-		    	 pr1.destroy();
-		    	 pr2.destroy();
-		    	 pr3.destroy();
-		} 
-		catch (IOException e) {
+
+			fstream = new FileInputStream(fileSynclogspath.substring(2) + logFileName);
+
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(fstream))) {
+
+				String strLine;
+				/* read log line by line */
+				while ((strLine = br.readLine()) != null) {
+					/* parse strLine to obtain what you want to read */
+					if (strLine.contains("ERROR")) {
+						pr1.destroy();
+						pr2.destroy();
+						pr3.destroy();
+
+						logger.info("File Sync activity Failed. Log File Name - " + logFileName.substring(1));
+						return "File Sync activity Failed. Log File Name - " + logFileName.substring(1);
+					}
+
+				}
+
+			}
+			pr1.destroy();
+			pr2.destroy();
+			pr3.destroy();
+		} catch (IOException e) {
 			logger.error(e.getMessage());
-			logger.info("File Sync activity Failed. Log File Name - "+logFileName.substring(1));
-			return "File Sync activity Failed. Log File Name - "+logFileName.substring(1);
-		}finally {
+			logger.info("File Sync activity Failed. Log File Name - " + logFileName.substring(1));
+			return "File Sync activity Failed. Log File Name - " + logFileName.substring(1);
+		} finally {
 			if (fstream != null)
-				 fstream.close();
-		
+				fstream.close();
+
 		}
-		
+
 		logger.info("File Sync activity Completed");
 		return "File Sync activity Completed";
 	}
-	
-	private void sleepProcess(Process bol)
-	{
-		//File f = new File(fileSynclogspath.substring(2) + logFileName);
-		//System.out.println("Log Folder Path - "+ f.getAbsolutePath());
+
+	private void sleepProcess(Process bol) {
+		// File f = new File(fileSynclogspath.substring(2) + logFileName);
+		// System.out.println("Log Folder Path - "+ f.getAbsolutePath());
 		while (bol.isAlive()) {
-			
+
 			try {
-				 logger.info("File Sync activity sleeping");
+				logger.info("File Sync activity sleeping");
 				Thread.sleep(1000);
-				 logger.info("File Sync activity wakeup");
+				logger.info("File Sync activity wakeup");
 			} catch (InterruptedException e) {
 				logger.error(e.getMessage());
-				 Thread.currentThread().interrupt();
+				Thread.currentThread().interrupt();
 			}
 		}
 	}
